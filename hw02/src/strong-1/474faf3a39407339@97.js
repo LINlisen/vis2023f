90c88062404e5768fb43845112b63b20d58ca4cc
LINlisen <49199663+LINlisen@users.noreply.github.com>
2023-11-17 14:46:34 +0800

    fix

--- hw02/src/strong-1/474faf3a39407339@97.js
+++ hw02/src/strong-1/474faf3a39407339@97.js
@@ -3,7 +3,7 @@ md`# HW2 Strong baseline-1`
 )}
 
 function _data(FileAttachment){return(
-FileAttachment("../data.json").json()
+FileAttachment("data.json").json()
 )}
 
 function _yCounts(){return(
@@ -86,7 +86,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["data.json", {url: new URL("./files/01d24d9aeda19b7590d996fac1553c965097547ebd857a93a21c0f86efc088993c6168f092b2353e6ec91202e5a6dead5ad66b688c1038a20deb6ec8d35e570d.json", import.meta.url), mimeType: "application/json", toString}]
+    ["data.json", {url: new URL("../data.json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
